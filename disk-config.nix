33ca71ad88ede1f02bbfcb75a9e4618441d0abd8
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2025-08-08 12:10:21 +0200

    config: update

--- disk-config.nix
+++ disk-config.nix
@@ -34,33 +34,20 @@
         };
       };
     };
-   disk.disk2 = {
+    disk.disk2 = {
       device = lib.mkDefault "/dev/nvme0n1";
       type = "disk";
       content = {
         type = "gpt";
         partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-            };
-          };
-          root = {
-            name = "root";
+          data = {
+            name = "data";
             size = "100%";
             content = {
-              type = "lvm_pv";
-              vg = "pool";
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/data";
+              mountOptions = [ "defaults" ];
             };
           };
         };
